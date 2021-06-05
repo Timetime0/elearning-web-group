7de9fcd27530a0e735b1c5438cf04f8459f9d7d3
huachung <huachung1304@gmail.com>
2021-06-05 21:33:54 +0700

    Update nav mai lam tiep

--- src/components/Header/NavHeader.jsx
+++ src/components/Header/NavHeader.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import useStyles from './NavHeaderStyle'
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import useStyles from "./NavHeaderStyle";
 import { withStyles } from "@material-ui/core/styles";
 
- function NavHeader(props) {
-  const {classes} = props
+import Icon from "@material-ui/core/Icon";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
+function NavHeader(props) {
+  const { classes } = props;
   return (
-    <div className={classes.nav}>
-      <AppBar position="static">
-        <Toolbar>
+    <div>
+      <AppBar position="static" className={classes.nav}>
+        <Toolbar className={classes.nav_Content}>
+          <div>
+            <Typography color="primary">
+              <h1>Elearning</h1>
+            </Typography>
+          </div>
+          <div className={classes.icon}>
+            <Typography color="primary">
+              <TwitterIcon className={classes.icon_Item} />
+            </Typography>
+            <Typography color="primary">
+              <FacebookIcon className={classes.icon_Item} />
+            </Typography>
+            <Typography color="primary">
+              <InstagramIcon className={classes.icon_Item} />
+            </Typography>
+          </div>
           <Typography color="primary" variant="h6" className={classes.title}>
             Home
           </Typography>
@@ -32,8 +52,15 @@ import { withStyles } from "@material-ui/core/styles";
           <Typography variant="h6" className={classes.title}>
             Contact
           </Typography>
-          <Button color="inherit">Login</Button>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <Typography variant="h6" className={classes.title}>
+            Login
+          </Typography>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
         </Toolbar>
@@ -41,4 +68,4 @@ import { withStyles } from "@material-ui/core/styles";
     </div>
   );
 }
-export default withStyles(useStyles, { withTheme: true })(NavHeader)
+export default withStyles(useStyles, { withTheme: true })(NavHeader);
--- src/components/Header/NavHeaderStyle.js
+++ src/components/Header/NavHeaderStyle.js
@@ -1,15 +1,30 @@
-const styles = theme => ({
+const styles = (theme) => ({
   nav: {
-    color: theme.palette.primary.dark
+    backgroundColor: "white",
+    margin: "0px",
   },
-
-  title:{
-    color: theme.palette.secondary.dark
+  nav_Content: {
+    display: "flex",
+    justifyContent: "center",
   },
-  menuButton:{
-    color: 'red'
+  icon: {
+    display: "flex",
+    verticalAlign: "10px",
+    margin: "0 30px",
   },
-
+  icon_Item: {
+    color: "#81868a",
+    padding: "0 5px",
+    fontSize: "18px",
+  },
+  title: {
+    color: "#81868a",
+    padding: "0px 20px",
+    fontSize: "14px",
+  },
+  menuButton: {
+    color: "yellow",
+  },
 });
 
-export default styles
+export default styles;
