0baf2a2258b86ac6afdd425ea69bdbcb0120ab50
huachung <huachung1304@gmail.com>
2021-07-04 20:56:23 +0700

    Update 4/7 lan 2

--- src/redux/saga/LoginSaga.js
+++ src/redux/saga/LoginSaga.js
@@ -1,6 +1,7 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { LoginService } from "../../services/LoginServices";
 import { LOGIN_SERVICE, LOGIN_SERVICE_SAGA } from "../types/LoginType";
+import Swal from 'sweetalert2'
 
 function* getLoginApi(action) {
   try {
@@ -12,6 +13,31 @@ function* getLoginApi(action) {
       data: res.data,
     });
     // popup
+    // switch(res.status){
+    //   case 200: {
+    //     return localStorage.setItem("user", JSON.stringify(res.data));
+    //   }
+    //   case 500: {
+    //      Swal.fire({
+    //                 icon: 'error',
+    //                 title: 'Tài khoản hoặc mật khẩu không chính xác!',
+    //             })
+    //   }
+    //   default: {
+    //     return true;
+    //   }
+    // }
+    if(res.status === 200){
+      Swal.fire({
+                    icon: 'success',
+                    title: 'Đăng nhập thành công',
+                })
+    }else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Tài khoản hoặc mật khẩu không chính xác!',
+                })
+            }
   } catch (e) {
     console.log(e);
     // popup
