5ecbbbe80940704940ca8a87b52e083a7ff0826c
Chung <huachung1304@gmail.com>
2021-07-10 17:30:13 +0700

    Update ngay 10/7

--- src/redux/reducers/LoginReducer.js
+++ src/redux/reducers/LoginReducer.js
@@ -1,11 +1,16 @@
 import { LOGIN_SERVICE } from "../types/LoginType";
+import { SIGN_UP } from "../types/SignUpType";
 
 const initialState = {
 	loginUser: [],
+	signupUser: [],
 }
 const LoginReducer = (state = initialState, action) => {
 	switch(action.type){
 		case LOGIN_SERVICE:{
+			return {...state, loginUser: action.data}
+		}
+		case SIGN_UP: {
 			return {...state, loginUser: action.data}
 		}
 		default:{
