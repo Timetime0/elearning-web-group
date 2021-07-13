439f6e05caab791fc9c0d360aec9cfecc096ca34
huachung <huachung1304@gmail.com>
2021-07-13 19:34:07 +0700

    Update toi 13/7

--- src/templates/client/TemplateClient.jsx
+++ src/templates/client/TemplateClient.jsx
@@ -7,14 +7,14 @@ import "../../assets/plugins/bootstrap/bootstrap.min.css";
 // import "../../Assets/";
 import "../../assets/css/style.css";
 // =======
-// import "../../assets/plugins/bootstrap/bootstrap.min.css";
-// import "../../assets/css/animate.min.css";
-// import "../../assets/plugins/fontawesome-free-5.11.2-web/css/all.min.css";
-// import "../../assets/plugins/kipso-icons/style.css";
-// import "../../assets/css/style.css";
-// import "../../assets/css/responsive.css";
+import "../../assets/plugins/bootstrap/bootstrap.min.css";
+import "../../assets/css/animate.min.css";
+import "../../assets/plugins/fontawesome-free-5.11.2-web/css/all.min.css";
+import "../../assets/plugins/kipso-icons/style.css";
+import "../../assets/css/style.css";
+import "../../assets/css/responsive.css";
 
-// import "../../_next/static/css/styles.b100dd23.chunk.css";
+import "../../_next/static/css/styles.b100dd23.chunk.css";
 
 // import "../../_next/static/agIWMwmAuC6_vhQUG9Afp/pages/index.js";
 // import "../../_next/static/agIWMwmAuC6_vhQUG9Afp/pages/_app.js";
